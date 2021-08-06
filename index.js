cd27bccb5d02e236c2f75605e8e3b555b4554c85
Alain Di Chiappari <alain.devcs@gmail.com>
2021-08-06 08:52:46 +0200

    working on schema

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { distance, closest } = require('fastest-levenshtein')
 const dice = require('fast-dice-coefficient')
+const Ajv = require("ajv")
+
+const candidatesSchema = require('./schema/candidates.json') 
 
 //console.log(distance('fast', 'faster'))
 //console.log(closest('fast', ['slow', 'faster', 'fastest']))
@@ -19,9 +22,13 @@ class QuickMatch {
     }
     if (!DISTANCE_ALGORITHMS[this.options.distanceAlgorithm]) throw new Error('Not valid distance algorithm given')
     this.algorithm = DISTANCE_ALGORITHMS[this.options.distanceAlgorithm] 
+
+    const ajv = new Ajv()
+    this.candidatesValidator = ajv.compile(candidatesSchema)
   }
 
   run (src, candidates) {
+    if (!this.candidatesValidator(candidates)) throw new Error('Candidates has not a valid format!')
     console.log('Algorithm:', this.options.distanceAlgorithm)
     for (const c of candidates) {
       const res = this.algorithm(src, c)
@@ -35,6 +42,9 @@ const qmd = new QuickMatch({ distanceAlgorithm: 'dice' })
 const qml = new QuickMatch({ distanceAlgorithm: 'levenshtein' })
 
 qmd.run('pizza', ['pezza', 'pane', 'pezzo'])
+qml.run('pizza', ['pezza', 'pane', 'pezzo'])
+ 
+qmd.run('pizza', [{ text: 'pezza' }, { text: 'pane', keywords: [] }, { text: 'pezzo' }])
 qml.run('pizza', ['pezza', 'pane', 'pezzo'])
  
 qmd.run('test con la pizza', ['voglio la pezza', 'test per il pane', 'con un test di pezzo'])
